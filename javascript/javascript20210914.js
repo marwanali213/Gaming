3b99d633f125c584d42422a5697231e4dd395851
Mariam Saad <103897877+Mariam-Saad512003@users.noreply.github.com>
2022-05-07 03:20:07 +0200

    Add files via upload

---
+++ javascript/javascript20210914.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  alert("this game only for +18");
+}
+
+
+function myFunction1() {
+  alert("Sorry This Prpperty Is Not Ready Yet !");
+}
+
+
+
+function mOver_(text) {
+  text.innerHTML = "Set in the post-apocalyptic United States,the game tells the story of survivors Joel and  Ellie as they work together to survive  their westward journey across what remains of the country to find a possible cure for the modern fungal plague that has nearly  decimated the entire human race."
+}
+function mOut_(text) {
+  text.innerHTML = "Mouse Over Me if you want to read more..."
+}
+
+function mhOver_(text) {
+  text.innerHTML = "Bridget cries over Sam s fetal corpse and we are then shown a dead baby on a beach Amelies beach. She picks him up and fixes the gunshot wound leaving him with his cross shaped scar.and bringing Sam back to life from death!"
+}
+function mhOut_(text) {
+  text.innerHTML = "if you over on this text you will know the end of death stranding!"
+}
