694124ff495ddfb70aaf700cf7ed6315c6b80400
mariamali002 <93216111+mariamali002@users.noreply.github.com>
2022-05-07 03:24:00 +0200

    Add files via upload

---
+++ javascript/p2.js
@@ -0,0 +1,29 @@
+function changeMode() {
+         var element = document.body;
+         element.classList.toggle("dark-mode");
+      }
+
+      function mUp(obj) {
+        obj.style.backgroundColor="#cbb4b4";
+
+        obj.innerHTML="Thanks";
+      }
+      function mdown(obj) {
+        obj.style.backgroundColor="#cbb4b4";
+
+        obj.innerHTML= "contact us";
+      }
+      function myFunction() {
+
+    let x = document.getElementById("numb").value;
+
+    let text;
+    if (isNaN(x) ||x>5) {
+      text = "awesome";
+    } else {
+      text = "contact us ";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+  function changeText(id) {
+    id.style.color = "red";}
