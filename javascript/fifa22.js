84338923677e5fe368c62795d055908e6b99d7fa
Marwan Ali <96815791+marwanali213@users.noreply.github.com>
2022-05-07 14:33:14 +0200

    Add files via upload

---
+++ javascript/fifa22.js
@@ -0,0 +1,9 @@
+function submitalert(){
+    alert("Your form is submitted successfully .");
+}
+function transfero(obj){
+    obj.innerHTML = "A Market that gives the players to buy ultimate team players , consumbles , contracts , kits and more"
+}
+function transferou(obj){
+    obj.innerHTML = "Fifa 22 Transfer Market"
+}
