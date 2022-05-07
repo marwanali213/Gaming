90005ce8e86e14d64219265b4f6930fdc52ffd53
mohamedhisham2212 <104698715+mohamedhisham2212@users.noreply.github.com>
2022-05-07 16:14:48 +0200

    Add files via upload

---
+++ javascript/dead-space.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('body');
+toggle002.onclick=function(){
+    toggle002.classList.toggle('active')
+    body.classList.toggle('active')
+    C002.classList.toggle('active')
+    nv002.classList.toggle('active')
+    side002.classList.toggle('active')
+    fo002.classList.toggle('active')
+    // li.classList.toggle('active')
+}
+function myFunction() {
+    var x = document.getElementById("form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
