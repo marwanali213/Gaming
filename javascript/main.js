0f23306affd83f5822cdd799d4d2765873a309f9
MarwanMet123 <104504794+MarwanMet123@users.noreply.github.com>
2022-05-10 10:41:42 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -68,21 +68,3 @@ var content = document.getElementsByTagName("body")[0];
 		    darkMode.classList.toggle("active");
 			content.classList.toggle("night");
 		})();
-
-var countDownDate = new Date("May 12, 2022 15:00:00").getTime();
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countDown").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
-
