e35224ade600dc30e45f75f24dd643d582997b17
mohamedhisham2212 <104698715+mohamedhisham2212@users.noreply.github.com>
2022-05-11 00:47:29 +0200

    Add files via upload

--- javascript/Bioshock.js
+++ javascript/Bioshock.js
@@ -1,18 +1,28 @@
 const body = document.querySelector('body');
-toggle.onclick=function(){
-    toggle.classList.toggle('active')
-    body.classList.toggle('active')
-    c.classList.toggle('active')
-    nv1.classList.toggle('active')
-    side.classList.toggle('active')
-    fff.classList.toggle('active')
-    li.classList.toggle('active')
+function lightm() {
+  toggle.classList.toggle('active')
+  body.classList.toggle('active')
+  c.classList.toggle('active')
+  nv1.classList.toggle('active')
+  side.classList.toggle('active')
+  fff.classList.toggle('active')
 }
-function myFunction() {
-    var x = document.getElementById("fr001");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+function appear() {
+  var x = document.getElementById("fr001");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
+function age() {
+  document.getElementById("ol").style.display = "block";
+}
+
+function off() {
+  document.getElementById("ol").style.display = "none";
+}
+function scfunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
